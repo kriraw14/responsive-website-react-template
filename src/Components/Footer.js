b449716cbba96e282406019da0f1145203a2397c
kriraw14 <krishan.rawat@outlook.com>
2023-11-02 22:52:05 +1300

    Added components
    Added multiple sections for template
    Footer
    Home
    Navbar
    Work

---
+++ src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Qualtiy</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Carrers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>+64-xxxx-xxxx</span>
+          <span>hello@xxxxx.com</span>
+          <span>contact@xxx.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
